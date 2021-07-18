0981abcdd3c0ef89ec6211ef096c1e8f0a9ea8b7
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-07-18 16:04:29 +0300

    v12

--- projects/ngneat/transloco-persist-translations/src/lib/helpers.ts
+++ projects/ngneat/transloco-persist-translations/src/lib/helpers.ts
@@ -1,11 +1,11 @@
 import { isObservable, from, of, Observable } from 'rxjs';
 import { isFunction } from '@ngneat/transloco';
 
-export function isPromise(v: any) {
+export function isPromise(v: any): v is Promise<any> {
   return v && isFunction(v.then);
 }
 
-export function observify<T>(asyncOrValue: any): Observable<T> {
+export function observify(asyncOrValue: any): Observable<any> {
   if (isPromise(asyncOrValue) || isObservable(asyncOrValue)) {
     return from(asyncOrValue);
   }
